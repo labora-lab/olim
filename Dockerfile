77c3d5bb5c05a7e6a1756d26a594f2499afa31d8
Nano Gennari <me@nngn.net>
2023-04-22 17:38:14 -0300

    Fixed port on Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -18,4 +18,4 @@ COPY . /app
 USER root
 
 # During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "rotulador:app"]
+CMD ["gunicorn", "--bind", "0.0.0.0:42000", "rotulador:app"]
--- rotulador/settings.py
+++ rotulador/settings.py
@@ -1,3 +1,5 @@
+import os
+
 ES_INDEX = "patients-texts"
 """Elasticserach index to load patient data"""
 
@@ -7,5 +9,6 @@ ES_LABEL_INDEX = "labels"
 ES_TO_HIDE_INDEX = "hidden-texts"
 """Elasticserach index to store texts to hide everywhere"""
 
-ES_SERVER_FILE = "server_credentials.json"
-"""Elasticsearch server credentials file"""
+ES_SERVER = os.getenv("ES_SERVER")
+if ES_SERVER == "":
+    ES_SERVER = "http://rotulador_es:9200/"
