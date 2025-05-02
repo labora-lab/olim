acccebebbc2406cab2b00688b8ef56580863e6b8
Felipe Adeildo <contato@felipeadeildo.com>
2025-05-02 03:32:51 -0300

    chore: celery-types dependency

--- olim/celery_app.py
+++ olim/celery_app.py
@@ -1,7 +1,10 @@
 import os
 
 from celery import Celery
+from celery.app.task import Task
 from dotenv import load_dotenv
+
+Task.__class_getitem__ = classmethod(lambda cls, *args, **kwargs: cls)  # type: ignore[attr-defined]
 
 load_dotenv()
 redis_port = os.getenv("REDIS_PORT", 6379)
@@ -12,12 +15,10 @@ redis_password = os.getenv("REDIS_PASSWORD", "")
 broker_url = f"redis://:{redis_password}@{redis_host}:{redis_port}/0"
 backend_url = f"redis://:{redis_password}@{redis_host}:{redis_port}/1"
 
-print(broker_url, backend_url)
-
-celery_app = Celery("olim", broker=broker_url, backend=backend_url, include=["olim.tasks"])
+app = Celery("olim", broker=broker_url, backend=backend_url, include=["olim.tasks"])
 
 
-celery_app.conf.update(
+app.conf.update(
     task_serializer="json",
     result_serializer="json",
     accept_content=["json"],
